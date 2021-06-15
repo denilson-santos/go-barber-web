a4ff2cdf843ffdcc32a2c5ec40fef6950e541de0
Denilson da Silva Santos <denilson.silvasantos.contato@gmail.com>
2021-06-14 21:47:11 -0300

    Config unform

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,17 +1,31 @@
-import React, { InputHTMLAttributes } from 'react';
+import { useField } from '@unform/core';
+import React, { InputHTMLAttributes, useEffect, useRef } from 'react';
 import { IconBaseProps } from 'react-icons/lib';
 
 import * as Style from './style';
 
 type InputProps = InputHTMLAttributes<HTMLInputElement> & {
+  name: string;
   icon?: React.ComponentType<IconBaseProps>;
 };
 
-const Input: React.FC<InputProps> = ({ icon: Icon, ...rest }) => (
-  <Style.Input>
-    {Icon && <Icon size={20} />}
-    <input {...rest} />
-  </Style.Input>
-);
+const Input: React.FC<InputProps> = ({ name, icon: Icon, ...rest }) => {
+  const inputRef = useRef(null);
+  const { fieldName, defaultValue, error, registerField } = useField(name);
 
+  useEffect(() => {
+    registerField({
+      name: fieldName,
+      ref: inputRef.current,
+      path: 'value',
+    });
+  }, [fieldName, registerField]);
+
+  return (
+    <Style.Input>
+      {Icon && <Icon size={20} />}
+      <input defaultValue={defaultValue} ref={inputRef} {...rest} />
+    </Style.Input>
+  );
+};
 export default Input;
--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { FiMail, FiLock, FiUser, FiArrowLeft } from 'react-icons/fi';
+import { Form } from '@unform/web';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
@@ -7,34 +8,40 @@ import * as Style from './style';
 
 import logoImg from '../../assets/logo.svg';
 
-const SignUp: React.FC = () => (
-  <Style.Container>
-    <Style.Background />
+const SignUp: React.FC = () => {
+  function handleSubmit(data: any): void {
+    console.log(data);
+  }
 
-    <Style.Content>
-      <img src={logoImg} alt="GoBarber" />
+  return (
+    <Style.Container>
+      <Style.Background />
 
-      <form>
-        <h1>Faça seu cadastro</h1>
+      <Style.Content>
+        <img src={logoImg} alt="GoBarber" />
 
-        <Input type="text" name="name" icon={FiUser} placeholder="Nome" />
-        <Input type="text" name="email" icon={FiMail} placeholder="E-mail" />
-        <Input
-          type="password"
-          name="password"
-          icon={FiLock}
-          placeholder="Senha"
-        />
+        <Form onSubmit={handleSubmit}>
+          <h1>Faça seu cadastro</h1>
 
-        <Button type="button">Cadastrar</Button>
-      </form>
+          <Input type="text" name="name" icon={FiUser} placeholder="Nome" />
+          <Input type="text" name="email" icon={FiMail} placeholder="E-mail" />
+          <Input
+            type="password"
+            name="password"
+            icon={FiLock}
+            placeholder="Senha"
+          />
 
-      <a href="/login">
-        <FiArrowLeft />
-        Fazer logon
-      </a>
-    </Style.Content>
-  </Style.Container>
-);
+          <Button type="submit">Cadastrar</Button>
+        </Form>
+
+        <a href="/login">
+          <FiArrowLeft />
+          Fazer logon
+        </a>
+      </Style.Content>
+    </Style.Container>
+  );
+};
 
 export default SignUp;
